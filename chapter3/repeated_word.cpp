0d8a757a56f13857e4a2ff88f870cf4b7bf17bea
Lida Strakh <strax69@gmail.com>
2022-12-16 10:21:19 +0100

    Chapter3: add program that detects repeated words.

---
+++ chapter3/repeated_word.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main() {
+  cout << "Enter your text: "<< '\n';
+  int number_of_words = 0;
+  string previous = " "; // not a word
+  string current;
+  while (cin >> current) {
+    ++number_of_words; // increase word count
+    if (previous == current) {
+      cout << "word number " << number_of_words
+           << " repeated: " << current << '\n';
+    }       
+    previous = current;
+  }
+  return 0;
+}
