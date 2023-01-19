86d95e8133bd3fe47d8b163ff7a1a9f44eb13b68
Lida Strakh <strax69@gmail.com>
2023-01-19 14:21:26 +0100

    Chapter3 ex8: add program that determines if integer is odd or even.

---
+++ chapter3/ex8_integer_odd_or_even.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+int main() {
+  int val = 0;
+  cout << "Type a number: ";
+  cin >> val;
+  if ((val2 % 2) == 0) {
+    cout << "The value " << val << " is an even number.\n";
+  } else {
+    cout << "The value " << val << " is an odd number.\n";
+  }
+  return 0;
+}
