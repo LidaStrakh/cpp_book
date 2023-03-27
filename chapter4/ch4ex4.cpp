a0cd2f2f73d99b88d73c163c379e364b639bb886
Lida Strakh <strax69@gmail.com>
2023-03-27 15:14:29 +0200

    Chapter4 ex4: add automatic function check.

--- chapter4/ch4ex4.cpp
+++ chapter4/ch4ex4.cpp
@@ -1,12 +1,16 @@
 #include "std_lib_facilities.h"
 #include <cassert>
 
-void guess(int* start, int* end) {
+bool verbose;
+
+void step(int* start, int* end, int x) {
   assert(*start < *end);
   string str;
   int mid = (*start + *end + 1) / 2;
-  cout << "Is the number you are thinking of less than " << mid <<"?(yes/no)\n";
-  cin >> str;
+  
+  //cout << "Is the number you are thinking of less than " << mid <<"?(yes/no)\n";
+  str = x < mid ? "yes" : "no";
+  //cout << str << "\n";
   
   if (str == "yes") {
     *end = mid - 1;
@@ -17,17 +21,25 @@ void guess(int* start, int* end) {
   }
 }
 
-int main() {
-  cout << "Think of a number between 1 and 100 and my program will figure out what the number is.\n";
-
+int guess(int x) {
   int start = 1;
   int end = 100;
-  
   while (start < end) {
-    guess(&start, &end);
+    step(&start, &end, x);
     cout << "start - " << start << " end - " << end << ".\n";
   }
-  
-  cout << "You are think of " << start << ".\n";
+  return start;
+}
 
+int main() {
+
+  for (int y = 1; y <= 100; ++y) {
+    cout << "!!!" << y  << "\n";
+    assert(y == guess(y));
+  }
+
+  int x;
+  cout << "Enter a number between 1 and 100 and my program will figure out what the number is.\n";
+  cin >> x;
+  cout << "You are think of " << guess(x) << ".\n";
 }
