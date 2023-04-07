e150e0e955234dcf48659bc19bdef0f58fed49fd
Lida Strakh <strax69@gmail.com>
2023-04-07 09:35:02 +0200

    Chapter4 ex4: add command line options to control verbose mode.
    The new options are -v | --verbose.

--- chapter4/ch4ex4.cpp
+++ chapter4/ch4ex4.cpp
@@ -1,17 +1,23 @@
 #include "std_lib_facilities.h"
 #include <cassert>
+#include <cstring>
 
-bool verbose;
+bool VERBOSE = false;
 
 void step(int* start, int* end, int x) {
   assert(*start < *end);
   string str;
   int mid = (*start + *end + 1) / 2;
-  
-  //cout << "Is the number you are thinking of less than " << mid <<"?(yes/no)\n";
+
+  if (VERBOSE) {
+    cout << "Is the number you are thinking of less than " << mid <<"?(yes/no)\n";
+  }
+
   str = x < mid ? "yes" : "no";
-  //cout << str << "\n";
-  
+
+  if (VERBOSE) {
+    cout << str << "\n";
+  }
   if (str == "yes") {
     *end = mid - 1;
   } else if (str == "no") {
@@ -26,20 +32,34 @@ int guess(int x) {
   int end = 100;
   while (start < end) {
     step(&start, &end, x);
-    cout << "start - " << start << " end - " << end << ".\n";
+    if (VERBOSE) {
+      cout << "start - " << start << "; end - " << end << ".\n";
+    }
   }
   return start;
 }
 
-int main() {
+int main(int argc, char** argv) {
+  if (argc == 1) {
+    VERBOSE = false;
+  } else if (argc == 2 && (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0)) {
+    VERBOSE = true;
+  } else {
+    cout << "Usage: " << argv[0] << " [-v | --verbose]\n";
+    return 1;
+  }
 
   for (int y = 1; y <= 100; ++y) {
-    cout << "!!!" << y  << "\n";
+    if (VERBOSE) {
+      cout << "!!!" << y  << "\n";
+    }
     assert(y == guess(y));
   }
+
 
   int x;
   cout << "Enter a number between 1 and 100 and my program will figure out what the number is.\n";
   cin >> x;
   cout << "You are think of " << guess(x) << ".\n";
+
 }
