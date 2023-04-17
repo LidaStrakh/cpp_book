b0e3a65a9f85c61d574bc7e4184f4764c169c2db
Lida Strakh <strax69@gmail.com>
2023-04-17 10:29:04 +0200

    Chapter4 ex2: add tests.

--- chapter4/ch4ex2.cpp
+++ chapter4/ch4ex2.cpp
@@ -1,24 +1,45 @@
 #include "std_lib_facilities.h"
+#include <cmath>
+#include <cassert>
+
+double median(const vector<double>* pnumbers) {
+  double med1;
+  size_t len = pnumbers->size();
+
+  if (len == 0) {
+    cout << "No numbers - no median!!!\n";
+    return NAN; // NAN - not a number
+  } else if (len % 2 != 0) {
+    med1 = (*pnumbers)[len / 2];
+  } else {
+    double med2 = (*pnumbers)[len / 2 - 1];
+    double med3 = (*pnumbers)[len / 2];
+    med1 = (med2 + med3) / 2;
+  }
+  return med1;
+}
+
+void test() {
+  vector<double> test1;
+  vector<double> test2 = {1, 2, 3};
+  vector<double> test3 = {1, 2, 3, 4};
+  assert(isnan(median(&test1)) != 0);
+  assert(median(&test2) == 2);
+  assert(median(&test3) == 2.5);
+}
 
 int main() {
+  test();
+
   double num;
+  double med;
   vector<double> numbers;
-  double median;
+
   cout << "Enter numbers.\n";
   while (cin >> num) {
     numbers.push_back(num);
   }
   sort(numbers.begin(), numbers.end());
-  size_t len = numbers.size(); 
-  if (len == 0) {
-    cout << "No numbers - no median!!!\n";
-    return 1;
-  } else if (len % 2 != 0) {
-    median = numbers[len / 2];
-  } else {
-    double med1 = numbers[len / 2 - 1];
-    double med2 = numbers[len / 2];
-    median = (med1 + med2) / 2;
-  }
-  cout << "Median is: " << median << ".\n";
+  med = median(&numbers);
+  cout << "Median is: " << med << ".\n";
 }
